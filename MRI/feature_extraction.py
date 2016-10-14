6b039b27718cedfa26ac92eaf23d3a3c2ef847be
Gisela <gisela.gabernet@pharma.ethz.ch>
2016-10-14 15:37:08 +0200

    calculating layer properties in feature_extraction class Contours

--- MRI/feature_extraction.py
+++ MRI/feature_extraction.py
@@ -1,26 +1,36 @@
 from skimage import measure
 import matplotlib.pyplot as plt
-import nibabel as nib
 import numpy as np
 
 class Contours:
-    def __init__(self, intensity, min_size):
-        self.contours = []
+    def __init__(self, intensity, min_size, layers_x_dim):
         self.intensity = intensity
         self.min_size = min_size
-
-    def get_2D_contours(self, X):
+        self.layers_x_dim = layers_x_dim
         self.contours = []
-        for n in range(len(X)):
-            all_contours = measure.find_contours(X[n], self.intensity)
-            sample_contours = []
-            for c in all_contours:
-                if c.shape[0] > self.min_size:
-                    sample_contours.append(c)
-            self.contours.append(sample_contours)
-        return self.contours
+        self.n_contours = []
+        self.biggest_cluster_size = []
+        self.descriptor = []
 
-    def plot_2D_contours(self, X):
+
+    def get_layer_contours_(self, layer):
+        self.layer_contours_ = []
+        all_contours = measure.find_contours(layer, self.intensity)
+        for c in all_contours:
+            if c.shape[0] > self.min_size:
+                self.layer_contours_.append(c)
+        return self.layer_contours_
+
+    def get_layer_contour_number_(self, layer):
+        contours = self.get_layer_contours_(layer)
+        self.n_contours = len(contours)
+        return self.n_contours
+
+    def get_layer_biggest_contour_area_(self):
+
+
+
+    def plot_layer_2D_contours_(self, X):
         for n in range(len(X)):
             fig, ax = plt.subplots()
             ax.imshow(X[n], interpolation='nearest', cmap=plt.cm.gray)
@@ -32,3 +42,5 @@ class Contours:
             ax.set_xticks([])
             ax.set_yticks([])
             plt.show()
+
+
