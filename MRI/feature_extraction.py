a0116cb4ee4a38659f6ae2fb95eb3c0ef680de1d
abutton <alexander.button@pharma.ethz.ch>
2016-10-14 15:49:09 +0200

    added get_contour_area into class Contours in feature extraction module

--- MRI/feature_extraction.py
+++ MRI/feature_extraction.py
@@ -14,21 +14,25 @@ class Contours:
 
 
     def get_layer_contours_(self, layer):
-        self.layer_contours_ = []
+        layer_contours_ = []
         all_contours = measure.find_contours(layer, self.intensity)
         for c in all_contours:
             if c.shape[0] > self.min_size:
-                self.layer_contours_.append(c)
-        return self.layer_contours_
+                layer_contours_.append(c)
+        return layer_contours_
 
     def get_layer_contour_number_(self, layer):
         contours = self.get_layer_contours_(layer)
-        self.n_contours = len(contours)
-        return self.n_contours
+        n_contours = len(contours)
+        return n_contours
 
-    def get_layer_biggest_contour_area_(self):
+    def get_contour_area_(self, contour):
+        x_coord = np.asarray(c[:, 0])
+        y_coord = np.asarray(c[:, 1])
+        y_next = np.roll(y_coord, -1)
+        y_diff = [abs(x) for x in (y_next - y_coord)]
 
-
+        return np.sum(np.prod([x_coord, y_diff], axis=0))
 
     def plot_layer_2D_contours_(self, X):
         for n in range(len(X)):
