0bf82211b572cdb139fbc6d0c11eebb023fb15ed
Leon-Klee <Leon.Bichmann@gmail.com>
2016-10-25 21:14:45 +0200

    feature extraction, model leon and submission changed

--- MRI/combined_model_leon_checkboard.py
+++ MRI/combined_model_leon_checkboard.py
@@ -9,11 +9,13 @@ from sklearn.pipeline import Pipeline
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.metrics import *
 from sklearn.linear_model import LassoCV
+from sklearn.linear_model import SGDRegressor
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVR
 from sklearn.decomposition import PCA, RandomizedPCA
 from itertools import chain
 from sklearn.feature_selection import SelectFromModel
+from sklearn.feature_selection import RFECV
 
 import matplotlib
 from matplotlib import pyplot as plt
@@ -31,12 +33,14 @@ print I.shape
 
 
 #X_train, X_test, y_train, y_test = \
-#    train_test_split(Data, Targets, test_size=0.33, random_state=42)
+ #   train_test_split(Data, Targets, test_size=0.33, random_state=42)
 X_train,y_train=Data,Targets
 
 cut = CenterCut()
 cut.make_cut(X_train)
 cut_train = cut.cut
+#cut.make_cubes(cut.cut, size_cubes=5)
+#desc_train = cut.descriptor
 
 check = CheckrPixl()
 checker = check.make_checker(cut_train)
@@ -50,6 +54,8 @@ desc_train = checker.checker
 
 #cut.make_cut(X_test)
 #cut_test = cut.cut
+#cut.make_cubes(cut.cut, size_cubes=5)
+#desc_test = cut.descriptor
 
 #filter.calculate_prewitt(cut_test)
 #desc_test2 = filter.flatten(filter.transformed)
@@ -67,12 +73,12 @@ print desc_train.shape
 pipe = Pipeline([('scl', StandardScaler()),
                  ('var', VarianceThreshold()),
                  ('pca', PCA(n_components=100)),
-                 ('clf', SVR(kernel='linear'))])
+                 ('clf', SVR())])
 
 param_range_svm = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1.0, 10, 100]
 param_range_lasso = np.linspace(0, 10, 11)
 gs = GridSearchCV(estimator=pipe,
-                  param_grid=[{'clf__C': [1.0], 'pca__n_components': [250]}],
+                  param_grid=[{'clf__C': [1], 'pca__n_components': [250]}],
                   cv=10,
                   n_jobs=1)
 
@@ -81,6 +87,7 @@ gs.fit(desc_train, y_train)
 best_pipe = gs.best_estimator_
 print gs.best_params_
 best_pipe.fit(desc_train, y_train)
+
 
 #score = best_pipe.score(desc_test, y_test)
 #print("Score R^2: "+str(score))
@@ -104,6 +111,9 @@ for i in range(1, 139):
 
 cut.make_cut(Data_test)
 cut_real_test = cut.cut
+#cut.make_cubes(cut.cut, size_cubes=5)
+#desc_real_test = cut.descriptor
+
 checker = check.make_checker(cut_real_test)
 desc_real_test = checker.checker
 
