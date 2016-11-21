6e2c4e7d6411989fca5eff31360e9146aa64b79a
Gisela <gisela.gabernet@pharma.ethz.ch>
2016-11-21 13:19:59 +0100

    solved issue in mutual information feature selection, in logloss calculation (needs probabilities and not class values)

--- MLP2/combined_model_optimization_gisela.py
+++ MLP2/combined_model_optimization_gisela.py
@@ -26,77 +26,90 @@ for i in range(1, 279):
 
 print I.shape
 
-X_train = Data
-y_train = Targets
-# X_train, X_test, y_train, y_test = \
-#      train_test_split(Data, Targets, test_size=0.33, random_state=42)
+Data = np.asarray(Data)
+print Data.shape
+
+X_train, X_test, y_train, y_test = \
+      train_test_split(Data, Targets, test_size=0.33, random_state=42, stratify=Targets)
+
+print X_train.shape
+print X_test.shape
+print y_train.shape
+print y_test.shape
+print y_test
 
 pipe = Pipeline([('cut', CenterCutCubes(size_cubes=3, plane_jump=1, x1=10, y1=10, z1=10, x2=170, y2=200, z2=170)),
                 ('var', VarianceThreshold()),
-                ('sel', Select(type='f_value', threshold=0.1)),
-                ('scl', StandardScaler()),
-                ('pca', PCA(n_components=250))])
+                ('sel', Select(type='mutual_info', threshold=0.1)),
+                ('scl', StandardScaler())
+                #('pca', PCA(n_components=250))
+                 ])
 
 
-# gs = GridSearchCV(estimator=pipe,
-#                   param_grid=[{'cut__size_cubes': [3],
-#                                'cut__x1': [10],
-#                                'cut__y1': [10],
-#                                'cut__z1': [10],
-#                                'cut__x2': [170],
-#                                'cut__y2': [200],
-#                                'cut__z2': [170]}],
+pipe.fit(X_train, y_train)
+X_train_t = pipe.transform(X_train)
+print X_train_t.shape
+#
+# svm_model = SVC(C=0.1, kernel='rbf', gamma=0.00001, class_weight={0: 3, 1: 1}, probability=True)
+#
+#
+# gs = GridSearchCV(estimator=svm_model,
+#                   param_grid={'C': [1, 0.1, 0.01],
+#                               'gamma': [1, 0.1, 0.001, 0.00001],
+#                               'kernel': ['linear', 'rbf']},
 #                   error_score=999,
 #                   cv=5,
 #                   n_jobs=-1,
 #                   verbose=10,
 #                   scoring=make_scorer(log_loss))
-
-pipe.fit(X_train, y_train)
-X_train_t = pipe.transform(X_train)
-
+#
+# gs.fit(X_train_t, y_train)
+#
 # means = gs.cv_results_['mean_test_score']
 # stds = gs.cv_results_['std_test_score']
 # for mean, std, params in zip(means, stds, gs.cv_results_['params']):
 #     print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
-
-#best_pipe = gs.best_estimator_
-#print gs.best_params_
-
-bagging = BaggingClassifier(base_estimator=SVC(C=0.1, kernel='rbf', gamma=0.00001, class_weight={0:3, 1:1}), n_estimators=10, n_jobs=-1, verbose=10, max_samples=0.5)
-
-#best_pipe.fit(X_train, y_train)
-bagging.fit(X_train_t, y_train)
-
+#
+# best_estim = gs.best_estimator_
+# best_estim.fit(X_train_t, y_train)
+# print gs.best_params_
+#
+#
 # X_test_t = pipe.transform(X_test)
 #
-# #y_test_predicted = best_pipe.predict(X_test)
-# y_test_predicted = bagging.predict(X_test_t)
+# y_test_predicted = best_estim.predict_proba(X_test_t)
 # score_test = log_loss(y_test, y_test_predicted)
 # print("log_loss test data: " + str(score_test))
-
-
-#y_train_predicted = best_pipe.predict(X_train)
-y_train_predicted = bagging.predict(X_train_t)
-score_train = log_loss(y_train, y_train_predicted)
-print("log_loss train data: " + str(score_train))
-
-
-Data_test = []
-for i in range(1, 139):
-    imagefile = nib.load("data/set_test/test_"+str(i)+".nii")
-    image = imagefile.get_data()
-    I = image[:, :, :, 0]
-    Data_test.append(np.asarray(I))
-
-X_test = Data_test
-X_test_t = pipe.transform(X_test)
-
-#predictions = best_pipe.predict(X_test)
-predictions = bagging.predict(X_test_t)
-
-with open("Scores.csv", mode='w') as f:
-    f.write("ID,Prediction\n")
-    for idx, pred in enumerate(predictions):
-        pred = round(pred, 0)
-        f.write(str(idx+1)+','+str(pred)+'\n')
+#
+#
+# y_train_predicted = best_estim.predict_proba(X_train_t)
+# score_train = log_loss(y_train, y_train_predicted)
+# print("log_loss train data: " + str(score_train))
+# scoreMCC_train = matthews_corrcoef(y_train, y_train_predicted)
+# print("MCC score train data: " + str(scoreMCC_train))
+#
+#
+# # Training model again with all the training data (without split)
+# X_train = Data
+# y_train = Targets
+#
+# X_train_t = pipe.transform(X_train)
+# best_estim.fit(X_train_t, y_train)
+#
+# # Generating score file for Kaggle test data
+# Data_test = []
+# for i in range(1, 139):
+#     imagefile = nib.load("data/set_test/test_"+str(i)+".nii")
+#     image = imagefile.get_data()
+#     I = image[:, :, :, 0]
+#     Data_test.append(np.asarray(I))
+#
+# X_test = Data_test
+# X_test_t = pipe.transform(X_test)
+#
+# predictions = best_estim.predict_proba(X_test_t)
+#
+# with open("Scoresbagging.csv", mode='w') as f:
+#     f.write("ID,Prediction\n")
+#     for idx, pred in enumerate(predictions):
+#         f.write(str(idx+1)+','+str(pred[1])+'\n')
