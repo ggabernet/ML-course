cd17129aabf38ff414ab53cdf76aae3386c21040
Gisela <gisela.gabernet@pharma.ethz.ch>
2016-11-22 16:50:35 +0100

    Last version of combined model optimization and model with bagging

--- MLP2/combined_model_bagging_gisela.py
+++ MLP2/combined_model_bagging_gisela.py
@@ -11,7 +11,6 @@ from sklearn.metrics import *
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
 from sklearn.decomposition import PCA
-from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import BaggingClassifier
 import matplotlib.pyplot as plt
 
@@ -43,44 +42,34 @@ pipe = Pipeline([('cut', CenterCutCubes(size_cubes=2, plane_jump=1)),
                 ('var', VarianceThreshold(0.01)),
                 ('sel', Select(type='mutual_info', threshold=0.1)),
                 ('scl', StandardScaler()),
-                ('pca', PCA(n_components=250))])
+                ('pca', PCA(n_components=150))])
 
 
 pipe.fit(X_train, y_train)
 X_train_t = pipe.transform(X_train)
 print X_train_t.shape
 
+bagging = BaggingClassifier(base_estimator=SVC(C=0.00001, kernel='linear', class_weight={0: 6, 1: 1}, probability=True),
+                            n_estimators=10,
+                            max_samples=0.5,
+                            max_features=1.0,
+                            bootstrap=True,
+                            n_jobs=-1,
+                            random_state=42,
+                            verbose=1)
 
-gs = GridSearchCV(estimator=SVC(probability=True),
-                  param_grid={'C': [0.0001],
-                              'kernel': ['linear'],
-                              'class_weight': [{0: 9, 1: 1}]},
-                  error_score=999,
-                  cv=5,
-                  n_jobs=-1,
-                  verbose=10,
-                  scoring=make_scorer(matthews_corrcoef))
 
-gs.fit(X_train_t, y_train)
-
-means = gs.cv_results_['mean_test_score']
-stds = gs.cv_results_['std_test_score']
-for mean, std, params in zip(means, stds, gs.cv_results_['params']):
-    print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
-
-best_estim = gs.best_estimator_
-best_estim.fit(X_train_t, y_train)
-print gs.best_params_
+bagging.fit(X_train_t, y_train)
 
 
 X_test_t = pipe.transform(X_test)
 
-y_test_predicted = best_estim.predict_proba(X_test_t)
+y_test_predicted = bagging.predict_proba(X_test_t)
 score_test = log_loss(y_test, y_test_predicted)
 print("log_loss test data: " + str(score_test))
 
 
-y_train_predicted = best_estim.predict_proba(X_train_t)
+y_train_predicted = bagging.predict_proba(X_train_t)
 score_train = log_loss(y_train, y_train_predicted)
 print("log_loss train data: " + str(score_train))
 
@@ -90,7 +79,7 @@ X_train = Data
 y_train = Targets
 
 X_train_t = pipe.transform(X_train)
-best_estim.fit(X_train_t, y_train)
+bagging.fit(X_train_t, y_train)
 
 # Generating score file for Kaggle test data
 Data_test = []
@@ -103,9 +92,9 @@ for i in range(1, 139):
 X_test = Data_test
 X_test_t = pipe.transform(X_test)
 
-predictions = best_estim.predict_proba(X_test_t)
+predictions = bagging.predict_proba(X_test_t)
 
-with open("Scores_optimization.csv", mode='w') as f:
+with open("Scores_bagging.csv", mode='w') as f:
     f.write("ID,Prediction\n")
     for idx, pred in enumerate(predictions):
         f.write(str(idx+1)+','+str(pred[1])+'\n')
