5f2c5cdb833a9c9f969b1c64e48fce403c554653
abutton <alexander.button@pharma.ethz.ch>
2016-12-11 17:58:36 +0100

    adding the class seperated SVC method

--- MLP3/multi_split.py
+++ MLP3/multi_split.py
@@ -33,54 +33,65 @@ for lines in target_file:
     lines = lines.split(',')
     Targets.append(lines)
 
-Targets = np.asarray(Targets,dtype=int)
+Targets = np.asarray(Targets)
 
 Data = []
 for i in range(1, 279):
     imagefile = nib.load("data/set_train/train_"+str(i)+".nii")
     image = imagefile.get_data()
-    I = image[:,:,80,0]
-    I = I.flatten(order='C')
+    I = image[:,:,:,0]
     imagefile.uncache()
     Data.append(np.asarray(I))
 
 Data = np.asarray(Data)
 
-#X_train, X_test, y_train, y_test = \
-#      train_test_split(Data, Targets, test_size=0.33, random_state=42)#, stratify=Targets)
+X_train, X_test, y_train, y_test = \
+      train_test_split(Data, Targets, test_size=0.33, random_state=42)#, stratify=Targets)
 
-X_train = Data
-y_train = Targets
+#X_train = Data
+#y_train = Targets
 
 print 'Fitting process started'
 
-forest = SVC(kernel='linear',C=0.1)
+forest = RandomForestClassifier(n_estimators=1, random_state=1)
 
 clf = MultiOutputClassifier(forest)
 
-pipe = Pipeline([#('cut', CenterCutCubes(size_cubes=10, plane_jump=1, x1=50, y1=80, z1=50, x2=120, y2=150, z2=100)),
-                  #('var', VarianceThreshold()),
-                  #('sel', Select(type='mutual_info', threshold=0.1)),
-                  #('scl', StandardScaler()),
-                  #('pca', PCA(n_components=50)),
-                  ('clf',clf)])
-print 'pipe done'
+pipe_list=[]
 
-pipe.fit(X_train,y_train)
+for Class in np.transpose(Targets):
+    Class = np.transpose(Class)
 
-print "Train - hamming loss :", hamming_loss(y_train.flatten(),pipe.predict(X_train).flatten())
-#print "Test - hamming loss :", hamming_loss(y_test.flatten(),pipe.predict(X_test).flatten())
+    pipe = Pipeline([('cut', CenterCutCubes(size_cubes=5, plane_jump=1, x1=50, y1=80, z1=50, x2=120, y2=150, z2=100)),
+                      #('var', VarianceThreshold()),
+                      #('sel', Select(type='mutual_info', threshold=0.1)),
+                      #('scl', StandardScaler()),
+                      #('pca', PCA(n_components=5)),
+                      ('clf',clf)])
+    print 'pipe done'
+    pipe.fit(X_train,y_train)
+
+    print "Train - hamming loss :", hamming_loss(y_train.flatten(),pipe.predict(X_train).flatten())
+    print "Test - hamming loss :", hamming_loss(y_test.flatten(),pipe.predict(X_test).flatten())
+
+    pipe_list.append(pipe)
 
 Data_test = []
 for i in range(1, 139):
     imagefile = nib.load("data/set_test/test_"+str(i)+".nii")
     image = imagefile.get_data()
-    I = image[:,:,80,0]
-    I = I.flatten(order='C')
+    I=image[:,:,:,0]
     imagefile.uncache()
     Data_test.append(np.asarray(I))
 
-output_data = pipe.predict(Data_test)
+output_data = []
+
+for index in range(0,3):
+    output_data.append(pipe_list[index].predict(Data_test))
+
+output_data=np.asarray(output_data)
+output_data = np.column_stack((output_data[0],output_data[1],output_data[2]))
+print output_data.shape
 
 
 ###########################################
@@ -90,7 +101,6 @@ output_data = pipe.predict(Data_test)
 #GENDER (gender is not actually binary - progressive thought leads to a progressive society
 
 gender = output_data[:,0]
-
 age = output_data[:,1]
 health = output_data[:,2]
 
